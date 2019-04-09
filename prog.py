680ba83cf70a7625d206f88201fd3eae845228a5
Katrina <kkshafer@umich.edu>
2019-04-09 11:20:13 -0400

    new comments

--- prog.py
+++ prog.py
@@ -1,14 +1,11 @@
 import random
+import json
 
 
 def num_vowels(word):
-    vowels = 'aeiou'
-    num = 0
-    for x in word:
-        if x in vowels:
-            num += 1
-    return num
-
-
-
-print(num_vowels("woooord"))
+    vowels = 'aeiou' ## defines vowels
+    num = 0 ## initializes number of vowels
+    for x in word: ## for each letter in the input word
+        if x in vowels: ## if the letter is a vowel
+            num += 1 ## sum vowels
+    return num ## return number of vowels
